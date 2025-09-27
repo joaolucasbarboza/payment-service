2a65e288da1a2d8d72cba7ffaec477b385e32326
joaobarboza <joaolucasbarbozaa@gmail.com>
2025-09-27 16:41:57 -0300

    initial commit

---
+++ src/main/java/com/joaobarboza/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarboza.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.joaobarboza.paymentservice")
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
+}
